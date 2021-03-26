5fd31a510783548e2ca86e46c0799a0debf5f13b
Anuj Tewari <anuj.tewari17>
2021-03-25 22:37:00 -0700

    Update Zero_shot_transformers.py

--- Zero_shot_transformers.py
+++ Zero_shot_transformers.py
@@ -12,13 +12,16 @@ from numpy import argmax
 #tool = language_tool_python.LanguageTool('en-US')
 zero_shot_classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
 
+#test.csv can just be a list of strings you want to classify
+df = pd.read_csv('test.csv')
+
 #Start out with creating a hypothesis template. The Null Hypothesis is status quo and the alternate is that text refers to the new topic.
 hypothesis_template = "This text is about {}."
 
 
 #creating a dictionary of topics and the subtopics that belong in each layer. This can also be done by creating a matrix of layers.
 thisdict = {
-    
+
     "A":'"a1","a2","a3"',
     "B":'"b1","b2","b3"',
     "C":'"c1","c2","c3"'
@@ -39,25 +42,25 @@ for i, row in df.iterrows():
     #fix grammar within each comment
     text = tool.correct(text)
     topic = row['topic']
-    
+
     #using classifier to generate hypothesis statements and testing against the data.
-    
+
     results = zero_shot_classifier(text, classes, hypothesis_template=hypothesis_template, multi_class=True)
-    
+
     #assigning scores
     SCORES = results["scores"]
     #assigning labels
     CLASSES = results["labels"]
-    
+
     #getting the highest score's index
-    
+
     BEST_INDEX = argmax(SCORES)
     predicted_class = CLASSES[BEST_INDEX]
     # Print sentence with predicted labels
     print("iter: "+text+" TOPIC:"+predicted_class)
-    
+
     #in this method We will be focusing on the top four topics and then explore the topmost subtopic within each. We can extend the logic as per the usecase.
-    
+
     #for first topic
     nc = thisdict[predicted_class]
     results_sub = zero_shot_classifier(text, nc, hypothesis_template=hypothesis_template, multi_class=True)
@@ -69,7 +72,7 @@ for i, row in df.iterrows():
     predicted_class_sub1 = CLASSES_sub[BEST_INDEX_sub]
     #print(predicted_class_sub)
     #print("iter: "+text+" sub topic:" + str(predicted_class_sub))
-    
+
     #for second topic
     predicted_class = CLASSES[BEST_INDEX+1]
     nc = thisdict[predicted_class]
@@ -80,8 +83,8 @@ for i, row in df.iterrows():
     predicted_class_sub2 = CLASSES_sub[BEST_INDEX_sub]
     #print(predicted_class_sub)
     #print("iter: "+text+" sub topic:" + str(predicted_class_sub))
-    
-    
+
+
     #for third topic
     predicted_class = CLASSES[BEST_INDEX+2]
     nc = thisdict[predicted_class]
@@ -92,21 +95,21 @@ for i, row in df.iterrows():
     predicted_class_sub3 = CLASSES_sub[BEST_INDEX_sub]
     #print(predicted_class_sub)
     #print("iter: "+text+" sub topic:" + str(predicted_class_sub))
-       
+
     #for fourth topic
     predicted_class = CLASSES[BEST_INDEX+3]
     nc = thisdict[predicted_class]
     results_sub = zero_shot_classifier(text, nc, hypothesis_template=hypothesis_template, multi_class=True)
-   
+
     SCORES_sub = results_sub["scores"]
     CLASSES_sub = results_sub["labels"]
     BEST_INDEX_sub = argmax(SCORES_sub)
     predicted_class_sub4 = CLASSES_sub[BEST_INDEX_sub]
     #print(predicted_class_sub)
     #print("iter: "+text+" sub topic:" + str(predicted_class_sub))
-    
-    #append everything to the dataframe. 
-        
+
+    #append everything to the dataframe.
+
     d.append(
             {
                 'text': row['text'],
@@ -123,18 +126,6 @@ for i, row in df.iterrows():
         )
 
     #output file write
-    
+
     output = pd.DataFrame(d)
     output.to_csv('topic-model-input-classified.csv', index=False)
-
-
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
